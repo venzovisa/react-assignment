2a1bd02da5a0a1411a3dca4f487f9e51de85a474
Venzo <nazobvane@gmail.com>
2025-06-13 16:20:42 +0300

    Setup store caching

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Link, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 import { Provider } from 'react-redux';
-import { setupStore } from './store/store.ts';
-import Posts from './components/posts/Posts.tsx';
-import Todos from './components/todos/Todos.tsx';
-import Result from 'antd/es/result/index';
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from './store/store.ts';
 import Navigation from './Navigation.tsx';
-import Home from './components/home/Home.tsx';
+import MainContent from './MainContent.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={setupStore()}>
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts/:userId" element={<Posts />} />
-          <Route path="/tasks" element={<Todos />} />
-          <Route path="*" element={
-            <Result
-              status="404"
-              title="404"
-              subTitle="Sorry, the page you visited does not exist."
-              extra={<Link to="/" className='button' title='Go back home'>Go back home</Link>}
-            />
-          } />
-        </Routes>
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Navigation />
+          <MainContent />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import usersReducer from "./reducers/usersSlice";
 import postsReducer from "./reducers/postsSlice";
 import todosReducer from "./reducers/todosSlice";
+import type { PersistPartial } from "redux-persist/es/persistReducer";
 
 export const rootReducer = combineReducers({
   users: usersReducer,
@@ -9,12 +12,28 @@ export const rootReducer = combineReducers({
   todos: todosReducer,
 });
 
-export const setupStore = (preloadedState?: Partial<RootState>) => {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const setupStore = (
+  preloadedState?: Partial<RootState> & Partial<PersistPartial>
+) => {
   return configureStore({
-    reducer: rootReducer,
-    preloadedState,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+    preloadedState: preloadedState as RootState & PersistPartial,
   });
 };
+
+export const store = setupStore();
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
